650d50200e0b5e3c0f9b51eb418de5f77ea91e61
sanghyup <hyupsang.dev@gmail.com>
2025-10-20 23:03:58 +0900

    feat(calculator): add console logging result

--- src/main/java/calculator/ConsoleRunner.java
+++ src/main/java/calculator/ConsoleRunner.java
@@ -12,7 +12,18 @@ public class ConsoleRunner {
     }
 
     public void run() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         double result = stringPlusCalculator.add(input);
+        printResult(result);
+    }
+
+    private void printResult(double result) {
+        System.out.print("결과 : ");
+        if (Double.isInfinite(result)) {
+            System.out.println(result);
+        } else {
+            System.out.println((int) result);
+        }
     }
 }
