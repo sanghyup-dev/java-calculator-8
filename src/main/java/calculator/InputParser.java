c6f23bd362161f59c5e8463c076b6ad532fd6be8
sanghyup <hyupsang.dev@gmail.com>
2025-10-20 22:23:48 +0900

    feat(inputparser): add custom delimiter parsing feature

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -6,18 +6,38 @@ import java.util.Set;
 
 public class InputParser {
 
-    private final Set<Character> delimiterSet = new HashSet<>();
+    private final Set<Character> commonDelimiterSet = new HashSet<>();
 
     public InputParser() {
-        delimiterSet.add(',');
-        delimiterSet.add(':');
+        commonDelimiterSet.add(',');
+        commonDelimiterSet.add(':');
     }
 
     public List<Double> parse(String input) {
         if (input == null) {
             throw new IllegalArgumentException("null 값이 입력되었습니다");
         }
+
+        Set<Character> delimiters = new HashSet<>(commonDelimiterSet);
+
+        if (hasCustomHeader(input)) {
+            input = applyCustomHeader(input, delimiters);
+        }
         System.out.println(input);
+        System.out.println(delimiters);
         return List.of(); // 임시 값
+    }
+
+    private boolean hasCustomHeader(String input) {
+        if (input.length() >= 5 && input.charAt(0) == '/' && input.charAt(1) == '/'
+                && input.charAt(3) == '\\' && input.charAt(4) == 'n') {
+            return true;
+        }
+        return false;
+    }
+
+    private String applyCustomHeader(String input, Set<Character> delimiters) {
+        delimiters.add(input.charAt(2));
+        return input.substring(5);
     }
 }
